2c58794689d40838eef85473cfc0506331f964f5
Yaroslav-Pa <yaroslav.roro@gmail.com>
2022-09-29 17:13:38 +0300

    29-09

--- bot.js
+++ bot.js
@@ -3,39 +3,123 @@ const Telegraf = require("telegraf").Telegraf;
         BOT_TOKEN = "5696222306:AAFnS77vQCXcE34mvpM5-iLxGCAzoMdEbs4";
 
 const bot = new Telegraf(BOT_TOKEN);
-let data_from_server = [];
+let data_from_server = {};
+let date_of_data_from_server ="";
+let KindOfStatistik = "stats";
 
-bot.start(ctx =>{
-    ctx.reply("Good day sir")
-    fetch("https://russianwarship.rip/api/v1/statistics/latest",
-    {
+function getCurrentDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today =  yyyy + '-' + mm + '-' + dd;
+    console.log(today);
+    return today;
+};
+
+async function getDataFromServer(forceFetch=false){
+    if (!forceFetch){
+        return;
+    }
+    return fetch("https://russianwarship.rip/api/v1/statistics/latest",{
         method:"GET",
         headers:{'Content-Type': 'application/json'}
-    })
+    })   
     .then((response) =>
     response.json()
     )
     .then ((data) =>
-        {
-           data_from_server = data.data.stats;
-        }
-    )
-    .catch((err) => {
-        console.log(`Error:${er}`);
+        { 
+            data_from_server=data;
+            date_of_data_from_server=data_from_server.data.date;
+            console.log("go to server");
+        })
+    .catch((er) => {
+        ctx.reply("Undefiend massage");
     })
+
+    // let responce = await fetch("https://russianwarship.rip/api/v1/statistics/latest",{
+    //     method:"GET",
+    //     headers:{'Content-Type': 'application/json'}
+    // });
+    // const data = await response.json();
+    // data.then ((data) =>
+    //     { 
+    //         data_from_server=data;
+    //         date_of_data_from_server=data_from_server.data.date;
+    //         console.log("go to server");
+    //     })
+    // .catch((er) => {
+    //     ctx.reply("Undefiend massage");
+    // })    
+}
+
+bot.start(ctx =>{
+    ctx.reply("Good day sir", {
+        reply_markup : {
+            inline_keyboard: [
+                [{text : "Resource", url: "https://russianwarship.rip/"}],
+                [{text : "Get latest data", callback_data: "getAll"},{text : "Get latest data by day", callback_data: "getAllByDay"}]
+            ]
+        }
+    });
+    // fetch("https://russianwarship.rip/api/v1/statistics/latest",
+    // {
+    //     method:"GET",
+    //     headers:{'Content-Type': 'application/json'}
+    // })
+    // .then((response) =>
+    // response.json()
+    // )
+    // .then ((data) =>
+    //     {
+    //        data_from_server = data.data.stats;
+    //     }
+    // )
+    // .catch((err) => {
+    //     console.log(`Error:${er}`);
+    // })
 })
+
+bot.action("getAll", ctx =>{
+    ctx.reply("DDDDDD");
+    KindOfStatistik = "stats";
+    // ctx.reply(data_from_server.data[KindOfStatistik]) 
+})
+bot.action("getAllByDay", ctx =>{
+    ctx.reply("ByDay");
+    KindOfStatistik = "increase";
+})
+
+
+ bot.command("/planes", ctx =>{
+     ctx.reply("show planes")
+ })
+ bot.command("/tanks", ctx =>{
+     ctx.reply("show tanks")
+ })
+
+
+
+
 bot.hears([/Hi+/i,"\u{1F600}"],(ctx) =>{
     ctx.reply("Anything i can do for you?")
 })
 
-bot.hears(/[A-Z]+/i,(ctx) =>{
+bot.hears(/[A-Z]+/i,async ctx =>{
     let massage = ctx.message.text;
-    
     console.log(massage);
-    switch (massage){
-        case "planes":ctx.reply(massage + ": " + data["data"]["stats"][massage]);break;
-        case "tanks":ctx.reply(massage + ": " + data["data"]["stats"][massage]);break;
-        default: ctx.reply("Undefiend massage");
-    }
+    // if (date_of_data_from_server!=getCurrentDate())
+    // {
+    await getDataFromServer(date_of_data_from_server!=getCurrentDate());
+    ctx.reply(massage + ": " + data_from_server.data[KindOfStatistik][massage]);
+    // });
+    
+    // }else
+    // {
+    //     ctx.reply(massage + ": " + data_from_server["data"]["stats"][massage]);
+    //     console.log("didnt go to server");
+    // }
 });
+
 bot.launch(); 
